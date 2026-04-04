ac192e400fa763078eb2d97421a914c59a493380
Suyono-Sukorame <suyonosukorame@ymail.com>
2026-04-04 14:45:26 +0800

    Add modern member registration form with wizard UI

--- Kode.js
+++ Kode.js
@@ -2,7 +2,14 @@ function onOpen(){
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('Arrahmah')
     .addItem('Home', 'fnHome')
+    .addItem('Pendaftaran Modern', 'fnDaftarAnggotaModern')
     .addToUi();
+}
+
+function fnDaftarAnggotaModern(){
+  var template = HtmlService.createTemplateFromFile('daftarAnggotaModern');
+  var htmlOutput = template.evaluate().setWidth(1200).setHeight(800);
+  SpreadsheetApp.getUi().showModalDialog(htmlOutput,'Pendaftaran Anggota (Modern)');
 }
 
 function fnHome(){
